da745be1c29deb5f0841de9d32840ee74ca2d7ac
Peperobles <josefedericorobles@gmail.com>
2019-02-04 08:37:46 +0100

    crud and list

--- src/components/AddressCrud/AddressCrud.js
+++ src/components/AddressCrud/AddressCrud.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 //Components
-import {Form} from '../Form/Form';
+import { Form } from '../Form/Form';
 
 export class AddressCrud extends Component {
 
@@ -19,41 +19,56 @@ export class AddressCrud extends Component {
     addUser = (firstName, lastName, email, country) => {
         // To update users an add to array
         this.setState(previousState => ({
-            users: [...previousState.users, {firstName, lastName, email, country}]
+            users: [...previousState.users, { firstName, lastName, email, country }]
         }))
+    }
 
-        console.log(this.state.users);
+    updateUser(){
+        console.log("HOLA")
+    }
+
+    deleteUser(key){
+        console.log(this.state.users[key]);
+        this.setState({ users: [...this.state.users, this.state.users[key]] });
+
+        // localStorage.removeItem('Contacts',key);
+        // this.setState(previousState => ({
+            
+        // }))
+        // console.log(key)
     }
 
     //Receive from localStorage
-    componentWillMount(){
+    componentWillMount() {
         localStorage.getItem('Contacts') && this.setState({
             users: JSON.parse(localStorage.getItem('Contacts'))
         })
     }
 
-
-
-
-
     // DEPRECATED ********************************** LOOK FOR OTHER WAY.
-    componentWillUpdate(nextProps, nextState){
+    componentWillUpdate(nextProps, nextState) {
         localStorage.setItem('Contacts', JSON.stringify(nextState.users))
     }
 
     render() {
+        // console.log(this.state.users);
         return (
             <div>
-                <Form addUser = {this.addUser}/>
-                <hr/>
+                <Form addUser={this.addUser} />
+                <hr />
                 <h6>USERS:</h6>
                 <ul>
-                 {this.state.users.map((user) =>
-
-                    <li key={user.email}>{user.firstName}{user.lastName}{user.email}{user.country}</li>
-                 )}   
+                    {this.state.users.map((user, index) =>
+                        <div key={index} id={user.email}>
+                            <h6>NAME: {user.firstName}</h6>
+                            <li>LAST NAME: {user.lastName}</li>
+                            <li>EMAIL: {user.email}</li>
+                            <li>COUNTRY: {user.country}</li>
+                            <button onClick={this.updateUser}>UPDATE</button> <button onClick={this.deleteUser.bind(this,index)}>DELETE</button>
+                        </div>
+                    )}
                 </ul>
-                
+
             </div>
         )
     }
--- src/components/AddressList/AddressList.js
+++ src/components/AddressList/AddressList.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export class AddressList extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [{}]
+    }
+  }
+
+  componentWillMount() {
+    localStorage.getItem('Contacts') && this.setState({
+      users: JSON.parse(localStorage.getItem('Contacts'))
+    })
+  }
   render() {
     return (
       <div>
         <h2>AQUI IRAN LOS CONTACTOS LEIDOS DESDE JSON</h2>
+        <ul>
+          {this.state.users.map((user, index) =>
+            <li key={index}>{user.firstName}{user.lastName}{user.email}{user.country}</li>
+          )}
+        </ul>
+
       </div>
     )
   }
